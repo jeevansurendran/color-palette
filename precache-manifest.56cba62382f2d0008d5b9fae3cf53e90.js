e22be3837471c79fbf5d8ad72123a26be7965f6a
Jeevan Surendran <jvns67@gmail.com>
2020-06-12 20:20:42 +0530

    Updates

---
+++ precache-manifest.56cba62382f2d0008d5b9fae3cf53e90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63fe9f06e8ac8a727fe35e86df82b99a",
+    "url": "/color-palette/index.html"
+  },
+  {
+    "revision": "4a23c79061861d013aa7",
+    "url": "/color-palette/static/css/main.e60648f2.chunk.css"
+  },
+  {
+    "revision": "6b7e23842d1a6987790a",
+    "url": "/color-palette/static/js/2.5ba9ed94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/color-palette/static/js/2.5ba9ed94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a23c79061861d013aa7",
+    "url": "/color-palette/static/js/main.baa5b753.chunk.js"
+  },
+  {
+    "revision": "7a2b36dd414b7e6d7229",
+    "url": "/color-palette/static/js/runtime-main.bc3b351f.js"
+  }
+]);
